d8380a3e5240908aacac7c51ca20dc927a604137
vivekrawat1998 <128046112+vivekrawat1998@users.noreply.github.com>
2026-04-22 15:23:56 +0530

    change

--- src/pages/Resourcepage.jsx
+++ src/pages/Resourcepage.jsx
@@ -9,7 +9,7 @@ import Typography from "../ui/Heading";
 import { trackDownload } from "../utils/trackDownload";
 import api from "../api/axios";
 
-const STRAPI_ORIGIN = import.meta.env.VITE_STRAPI_URL || "http://13.205.121.242";
+const STRAPI_ORIGIN = import.meta.env.VITE_STRAPI_URL || "https://sanseed.org";
 
 const getFileUrl = (url) => {
   if (!url) return "";
